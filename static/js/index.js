837604561d9d65e755912d17f609a3cb89d2d6f1
skrphenix <saikeerthan.keerthan.9@gmail.com>
2024-04-16 15:46:22 +0530

    initial commit

---
+++ static/js/index.js
@@ -0,0 +1,30 @@
+function disable_img_interaction(element_id) {
+    let image = document.getElementById(element_id);
+    image.style.pointerEvents = "none";
+}
+
+function get_copyright() {
+    let publish_year = 2024;
+    let cpy_elm = document.getElementById("copyright-year");
+
+    const d = new Date();
+    let curr_year = d.getFullYear();
+    if (curr_year > publish_year)
+        cpy_elm.innerHTML = `© ${publish_year}-${curr_year}`;
+    else
+        cpy_elm.innerHTML = `© ${curr_year}`;
+}
+
+function enable_tooltips() {
+    const tooltipTriggerList = document.querySelectorAll(
+        '[data-bs-toggle="tooltip"]'
+    );
+    const tooltipList = [...tooltipTriggerList].map(
+        (tooltipTriggerEl) =>
+            new bootstrap.Tooltip(tooltipTriggerEl)
+    );
+}
+
+document.addEventListener("DOMContentLoaded", (e) => { disable_img_interaction("about-banner") });
+document.addEventListener("DOMContentLoaded", (e) => { enable_tooltips() });
+document.addEventListener("DOMContentLoaded", (e) => { get_copyright() });
---
+++ static/js/scroll.js
@@ -0,0 +1,56 @@
+function inScrollView(elem) {
+    let rect = elem.getBoundingClientRect();
+    return rect.bottom - 50 <= window.innerHeight;
+}
+
+function view_scroll_btn(btn) {
+    if (window.scrollY < 300) {
+        btn.style.display = "none";
+    }
+    else {
+        btn.style.display = "block";
+    }
+}
+
+function add_view_to_elements() {
+    let about = document.getElementById("about");
+    if (inScrollView(about))
+        if (!about.classList.contains("view"))
+            about.classList.add("view");
+
+    let cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
+        let card = cards[i];
+        if (inScrollView(card))
+            if ((i + 1) % 2 !== 0) {
+                if (!(card.classList.contains("view") && card.classList.contains("odd")))
+                    card.classList.add("view", "odd");
+            }
+            else {
+                if (!(card.classList.contains("view") && card.classList.contains("even")))
+                    card.classList.add("view", "even");
+            }
+    }
+
+    let social_icons = document.querySelectorAll("#contact a.btn");
+    for (let i = 0; i < social_icons.length; i++) {
+        let icon = social_icons[i];
+        if (inScrollView(icon))
+            if (!icon.classList.contains("view"))
+                icon.classList.add("view");
+    }
+};
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    let btn = document.getElementById("scroll-top-btn");
+    btn.addEventListener("click", (e) => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        btn.style.display = "none";
+    });
+
+    view_scroll_btn(btn);
+    add_view_to_elements();
+
+    document.addEventListener("scroll", (e) => { view_scroll_btn(btn) });
+    document.addEventListener("scroll", (e) => { add_view_to_elements() });
+});
